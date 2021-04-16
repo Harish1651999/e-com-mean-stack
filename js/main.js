353872bc0cff7d77f7bff20a0ad1d75fe0962590
S.Harishkumar <hk1651999@gmail.com>
2021-04-16 21:31:21 +0530

    add cart page.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,36 @@ navToggle.addEventListener('click', function(){
  linkItem.classList.toggle('links');
  console.log("clicked");
 });
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var pageScroll = $(window).scrollTop();
+//         if(pageScroll > 250){
+//             $("#header").addClass("active-header");
+//             $(".sticky-logo").addClass("active-logo");
+//             $(".btm-nav-br").addClass("active-btm-nav");
+
+//         }else{
+//             $("#header").removeClass("active-header");
+//             $(".sticky-logo").removeClass("active-logo");
+//             $(".btm-nav-br").removeClass("active-btm-nav");
+//         }
+       
+//     })
+
+//     $("#hdrcart").mouseenter(function(){
+//         $(".hdr-crt-lst").slideDown(500);
+//     });
+//     $("#hdrcart").mouseleave(function(){
+//         $(".hdr-crt-lst").slideUp(500);        
+//     });
+// });
+
+    $(document).ready(function(){
+        $("#hdrcart").mouseenter(function(){
+            $(".hdr-crt-lst").slideDown(500);
+        });
+        $("#hdrcart").mouseleave(function(){
+            $(".hdr-crt-lst").slideUp(500);
+        });
+    })
